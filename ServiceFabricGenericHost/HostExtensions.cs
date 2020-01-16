8cf965d74ffaa2ada7d0ccfc2cebaea224725929
Allan Lindqvist <allan@funrock>
2020-01-16 12:05:22 +0100

    helper method to add asp.net service for regular services

--- ServiceFabricGenericHost/HostExtensions.cs
+++ ServiceFabricGenericHost/HostExtensions.cs
@@ -129,6 +129,24 @@ namespace ServiceFabricGenericHost
         }
     }
 
+    public static class ServiceFabricExtensions
+    {
+        public static ServiceReplicaListener HttpAspnetListener<TStartup>( this StatefulServiceContext serviceContext, string endpointName = "GrpcEndpoint" ) where TStartup : class
+        {
+            var ep = serviceContext.CodePackageActivationContext.GetEndpoint( endpointName );
+            return Host.CreateDefaultBuilder()
+                 .ConfigureWebHostDefaults( webhost => webhost
+                 .UseKestrel( options =>
+                 {
+                     options.Listen( IPAddress.Any, ep.Port, listenoptions => { listenoptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2; } );
+                     options.AllowSynchronousIO = true;
+                 } )
+                 .UseStartup<TStartup>()
+                 .UseEndPoints( ep ) )
+                 .ToReplicaListener( serviceContext.NodeContext, ep );
+        }
+    }
+
     public class FakeCommunicationListener : ICommunicationListener
     {
         public string Url { get; }
